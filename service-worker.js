7d97f8eb29b72b4c6abf944c12e1712e59bbc1ae
Nathan <nathanams.5d@gmail.com>
2023-08-26 19:38:28 -0400

    fix: pwa

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "intervalo-flex-cache-v1";
+const urlsToCache = [
+  "/",
+  "index.html",
+  "icon.png",
+  // List any other static assets you want to cache
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
