a46f74e90812bc61ffb15197defbd80fdab9d562
Nathan <nathanams.5d@gmail.com>
2023-08-27 07:33:31 -0400

    fix: pwa

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 const CACHE_NAME = "intervalo-flex-cache-v1";
+const CACHE_TIME = 5 * 24 * 60 * 60 * 1000; // 5 dias em milissegundos
+
 const urlsToCache = [
   "/IntervaloFlex/",
   "/IntervaloFlex/index.html",
@@ -16,8 +18,33 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((response) => {
+        const fetchPromise = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return (
+              cacheName.startsWith("intervalo-flex-cache-") &&
+              cacheName !== CACHE_NAME
+            );
+          })
+          .map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+      );
     })
   );
 });
