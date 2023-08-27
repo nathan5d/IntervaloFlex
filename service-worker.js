d8fc9e3eb4f03160bed5dd07230df1a820faa85f
Nathan <nathanams.5d@gmail.com>
2023-08-27 16:30:13 -0400

    fix: pwa

--- service-worker.js
+++ service-worker.js
@@ -2,50 +2,61 @@ const CACHE_NAME = "intervalo-flex-cache-v1";
 const CACHE_TIME = 5 * 24 * 60 * 60 * 1000; // 5 dias em milissegundos
 
 const urlsToCache = [
-  "/IntervaloFlex/",
-  "/IntervaloFlex/index.html",
-  "/IntervaloFlex/icon.png",
-  "/IntervaloFlex/service-worker.js",
-  // Adicione outros recursos que deseja cachear aqui
+    "/",
+    "/IntervaloFlex/",
+    "/IntervaloFlex/index.html",
+    "/IntervaloFlex/style.css",
+    "/IntervaloFlex/icon.png",
+    "/IntervaloFlex/logo.png",
+    // Adicione outros recursos que deseja cachear aqui
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache);
+        })
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(event.request).then((response) => {
-        const fetchPromise = fetch(event.request).then((networkResponse) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-        return response || fetchPromise;
-      });
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response;
+            }
+
+            return fetch(event.request).then((networkResponse) => {
+                if (!networkResponse || networkResponse.status !== 200) {
+                    return networkResponse;
+                }
+
+                const cacheResponse = networkResponse.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, cacheResponse);
+                });
+
+                return networkResponse;
+            });
+        })
+    );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((cacheName) => {
-            return (
-              cacheName.startsWith("intervalo-flex-cache-") &&
-              cacheName !== CACHE_NAME
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((cacheName) => {
+                        return (
+                            cacheName.startsWith("intervalo-flex-cache-") &&
+                            cacheName !== CACHE_NAME
+                        );
+                    })
+                    .map((cacheName) => {
+                        return caches.delete(cacheName);
+                    })
             );
-          })
-          .map((cacheName) => {
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
+        })
+    );
 });
